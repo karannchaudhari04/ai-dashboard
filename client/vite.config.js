f0f8e2f190095d3359f6fbebef37ddf71370ea67
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-29 17:06:56 +0530

    initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['fs', 'path'] // ← Add any Node.js built-ins here if causing issues
+    }
+  }
 })
